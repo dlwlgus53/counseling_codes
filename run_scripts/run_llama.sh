d5cdc92129d5a1ae64e93eae1571648af6f73f49
jihyunlee <dlwlgus53@users.noreply.github.com>
2026-06-08 02:12:26 +0900

    Remove unused root run scripts

--- run_scripts/run_llama.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-export OPENAI_API_KEY=""
-
-cd ..
-
-CUDA_VISIBLE_DEVICES=0 python -m llm_therapist.run \
-    --client_model_name gpt-3.5-turbo \
-    --counselor_model_path /home/kimsubin/model/Meta-Llama-3-8B-Instruct \
-    --input_data ../data/processed/test.csv
